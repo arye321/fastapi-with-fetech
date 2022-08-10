435f69039073168d40ba81c3216b87d576ba8fec
arye321 <arye321@users.noreply.github.com>
2022-08-10 05:27:34 +0000

    works same folder

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,38 @@
-from fastapi import FastAPI
+# uvicorn main:app --reload
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-
-
-todos = [
-    {
-        "id": "1",
-        "item": "Read a book."
-    },
-    {
-        "id": "2",
-        "item": "Cycle around town."
-    }
-]
-
+from starlette.responses import FileResponse 
 
 app = FastAPI()
+# app.mount("/", StaticFiles(directory="static",html = True), name="static")
 
-origins = [
-    "https://arye321-fastapi-with-fetech-x5jggrp5cp4v4-5501.githubpreview.dev"
-]
+# origins = [
+#     "https://arye321-fastapi-with-fetech-x5jggrp5cp4v4-5501.githubpreview.dev"
+# ]
 
 
-app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
-)
+# app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=origins,
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"]
+# )
 
+@app.post("/cookie")
+def create_cookie(response: Response,username: dict):
+    print(username)
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return {"message": "Come to the dark side, we have cookies"}
 
 @app.get("/")
 async def read_root() -> dict:
-    return {"message": "Welcome to your todo list."}
+    # return {"message": "Welcome to your todo list."}
+    return FileResponse('static/index.html')
 
 
 
 
 @app.post("/test")
 async def test(username: dict):
-    
     return f"lol {username}"
