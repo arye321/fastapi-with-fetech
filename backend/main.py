4aa4aad99e943ec967fd144e2fd6c75d219b42b0
arye321 <arye321@users.noreply.github.com>
2022-08-11 05:16:39 +0000

    showcase working

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from starlette.responses import FileResponse
 Trying fastapi_login package :)
 """
 from datetime import timedelta
-
+from fastapi.requests import Request
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_login.exceptions import InvalidCredentialsException
@@ -17,13 +17,20 @@ SECRET = 'f691b8d9c25109a525637a43d9b6399565cd7437f0c8bdb8'
 app = FastAPI()
 fake_db = {'asdf': {'password': 'asdf'}}
 
-
 manager = LoginManager(SECRET, token_url='/auth/token', use_cookie=True)
+manager.useRequest(app)
 
 @manager.user_loader()
 def load_user(email: str):  # could also be an asynchronous function
     user = fake_db.get(email)
     return user
+
+
+@app.get('/showcase')
+def showcase(request: Request):
+    # None if unauthorized
+    user = request.state.user
+    return f"{user}"
 
 @app.post('/auth/token')
 def login(response: Response,data: OAuth2PasswordRequestForm = Depends()):
@@ -36,7 +43,7 @@ def login(response: Response,data: OAuth2PasswordRequestForm = Depends()):
     elif password != user['password']:
         raise InvalidCredentialsException
     short_token = manager.create_access_token(
-        data=dict(sub=email), expires=timedelta(minutes=1)
+        data=dict(sub=email), expires=timedelta(days=1)
     )
     manager.set_cookie(response, short_token)
     return {'access_token': short_token, 'token_type': 'bearer'}
